3956ac4c2881a2da01092d243d7780fdf28e8dd8
thewittybrown <vibhubg844@gmail.com>
2020-12-23 18:37:30 +0530

    added navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,63 @@
-import './App.css';
+import React from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {fetchPosts} from '../actions/posts';
+import { PostsList } from './';
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+  render() {
+    const { posts } = this.props;
+    return (
+      <div>
+        <nav className="nav">
+          <div className="left-div">
+            <img src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png" alt="logo"/>
+          </div>
+          <div className="search-container">
+            <img className="search-icon" src="https://image.flaticon.com/icons/svg/483/483356.svg" alt="search-icon"/>
+            <input placeholder="Search"/>
+
+            <div className=" search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img src="https://image.flaticon.com/icons/svg/2154/2154651.svg" alt="user-dp"/>
+                  <span>JohnDoe</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-div">
+            <div className="user">
+              <img src="https://image.flaticon.com/icons/svg/2154/2154651.svg" alt="user-dp" id="user-dp"/>
+              <span>JohnDoe</span>
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>Log in</li>
+                <li>Log out</li>
+                <li>Register</li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <PostsList posts={posts} />
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps (state) {
+  return {
+    posts: state.posts,
+  };
+}
+
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
+
+export default connect(mapStateToProps)(App);
