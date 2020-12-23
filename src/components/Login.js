3d87007e6990cf9d62feafddcdbd525bc21a39af
thewittybrown <vibhubg844@gmail.com>
2020-12-24 03:28:37 +0530

    added controlled component file handling

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,15 +3,30 @@ import React, { Component } from 'react';
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.emailInputRef = React.createRef();
-    this.passwordInputRef = React.createRef();
+    // this.emailInputRef = React.createRef();
+    // this.passwordInputRef = React.createRef();
+    this.state = {
+      email: '',
+      password: '',
+    };
   }
 
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
   handleFormSubmit = (e) => {
-      e.preventDefault();
-      console.log('this.emailInputRef', this.emailInputRef);
-      console.log('this.passwordInputRef', this.passwordInputRef);
-  }
+    e.preventDefault();
+    //   console.log('this.emailInputRef', this.emailInputRef);
+    //   console.log('this.passwordInputRef', this.passwordInputRef);
+    console.log(this.state), this.state;
+  };
   render() {
     return (
       <form className="login-form">
@@ -21,7 +36,9 @@ class Login extends Component {
             type="email"
             placeholder="Email"
             required
-            ref={this.emailInputRef}
+            // ref={this.emailInputRef}
+            onChange={this.handleEmailChange}
+            value={this.state.email}
           />
         </div>
         <div className="field">
@@ -29,11 +46,13 @@ class Login extends Component {
             type="password"
             placeholder="Password"
             required
-            ref={this.passwordInputRef}
+            // ref={this.passwordInputRef}
+            onChange={this.handlePasswordChange}
+            value={this.state.password}
           />
         </div>
         <div className="field">
-          <button onClick={this.handleFormSubmit} >Log in</button>
+          <button onClick={this.handleFormSubmit}>Log in</button>
         </div>
       </form>
     );
