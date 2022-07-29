af9c4334a1159d7630deae5254b33270dc54a2f5
Vibhu B Gupta <70993927+vibhubgupta@users.noreply.github.com>
2022-07-29 23:11:13 +0530

    Update Page404.js

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -3,9 +3,11 @@ import React from 'react';
 function Page404(props) {
   return (
     <div>
-      <h1>Visit home page to enter your wittygram-> </h1>
+      <h1>Welcome to Wittygramm! Visit the home page to enter our network-> </h1>
       <br />
-      <h3>Kogin/Sign up to get started! </h3>
+      <h3>Login/Sign up to get started! </h3>
+      <br />
+      <p>If this page keeps popping up, it&apos;s most probably your own fault! ;) </p>
     </div>
   );
 }
