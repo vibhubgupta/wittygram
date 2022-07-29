4b819873b08b7c7dbaa825ed305be8ee3eb8f78e
vibhubgupta <vibhubg844@gmail.com>
2022-07-29 15:26:24 +0530

    Updates

---
+++ precache-manifest.0ae47628b9c234024f6af9769e7b0552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9645141ac78325c945c09a6450cae022",
+    "url": "/codeial-final-live/index.html"
+  },
+  {
+    "revision": "6498328fbea975395901",
+    "url": "/codeial-final-live/static/css/main.7b653a32.chunk.css"
+  },
+  {
+    "revision": "aa110bd25b06f3ba575f",
+    "url": "/codeial-final-live/static/js/2.ff6b42fa.chunk.js"
+  },
+  {
+    "revision": "fdca082fd0c4ef1db4a5b3f48c425822",
+    "url": "/codeial-final-live/static/js/2.ff6b42fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6498328fbea975395901",
+    "url": "/codeial-final-live/static/js/main.c829a4fa.chunk.js"
+  },
+  {
+    "revision": "a60458b3ccf8cb9e6bd5",
+    "url": "/codeial-final-live/static/js/runtime-main.134dafa4.js"
+  }
+]);
