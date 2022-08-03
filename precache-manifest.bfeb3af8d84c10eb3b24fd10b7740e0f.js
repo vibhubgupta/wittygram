31f951b395886424cf8297b9c617d5208f2ecdc2
vibhubgupta <vibhubg844@gmail.com>
2022-08-03 23:18:09 +0530

    Updates

---
+++ precache-manifest.bfeb3af8d84c10eb3b24fd10b7740e0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad64305a797df1187a6feee0fffd9e48",
+    "url": "/wittygram/index.html"
+  },
+  {
+    "revision": "4cda3e0847b75ae04d14",
+    "url": "/wittygram/static/css/main.7b653a32.chunk.css"
+  },
+  {
+    "revision": "d337f5b6e607d7e64a87",
+    "url": "/wittygram/static/js/2.ab2f8849.chunk.js"
+  },
+  {
+    "revision": "fdca082fd0c4ef1db4a5b3f48c425822",
+    "url": "/wittygram/static/js/2.ab2f8849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cda3e0847b75ae04d14",
+    "url": "/wittygram/static/js/main.4b5d0d3e.chunk.js"
+  },
+  {
+    "revision": "6869cbc213c3ed8e01f1",
+    "url": "/wittygram/static/js/runtime-main.7ff06426.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.cc6b03c5b7d37e5d46ea1ceaf89c9308.js"
+  "/wittygram/precache-manifest.bfeb3af8d84c10eb3b24fd10b7740e0f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wittygram/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
